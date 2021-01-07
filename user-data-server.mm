71a8e6dad25862802a3c3b2b852120b9f7edb0ef
Martin Jahr <martin@desixma.org>
2021-01-07 13:07:18 +0100

    unified inventory

--- user-data-server.mm
+++ user-data-server.mm
@@ -47,26 +47,40 @@ Content-Transfer-Encoding: 7bit
 Content-Disposition: attachment; filename="userdata.txt"
 #!/bin/bash
 
-useradd -m minetest
-usermod -L minetest
+# prepare host for container
 apt -y update
 apt -y install docker.io
+useradd -m minetest
+usermod -L minetest
+
+# allow external access
 ufw allow 30000/udp
 ufw allow 30000/tcp
 ufw reload
-mkdir -p /home/minetest/data
-chown 30000:30000 /home/minetest/data
+
+# create host dir for configuration
 mkdir -p /home/minetest/conf
 chown 30000:30000 /home/minetest/conf
 touch /home/minetest/conf/minetest.conf
-
 echo "active_block_range = 3" > /home/minetest/conf/minetest.conf
 echo "max_block_send_distance = 30" >> /home/minetest/conf/minetest.conf
 echo "max_block_generate_distance = 30" >> /home/minetest/conf/minetest.conf
 echo "client_unload_unused_data_timeout = 300" >> /home/minetest/conf/minetest.conf
 echo "client_mapblock_limit = 2000" >> /home/minetest/conf/minetest.conf
 echo "time_speed = 0" >> /home/minetest/conf/minetest.conf
-
 chown 30000:30000 /home/minetest/conf/minetest.conf
+
+# create host dir for worlds, mods etc.
+mkdir -p /home/minetest/data/.minetest/mods
+chown -R 30000:30000 /home/minetest/data
+
+# get mods
+cd /tmp
+wget https://github.com/minetest-mods/unified_inventory/archive/master.zip
+unzip master.zip
+rm master.zip
+mv unified_inventory-master /home/minetest/data/.minetest/mods/unified_inventory
+
+# run server
 docker run -p "30000:30000/udp" -e "PGID=30000" -e "PUID=30000"  -v /home/minetest/data/:/var/lib/minetest/ -v /home/minetest/conf:/etc/minetest/ registry.gitlab.com/minetest/minetest/server:025035db5c87e9eaa9f83859f860539fc4fb4dc0
 --//
